6c270ccf18a1c99a1d10353a970a131a8da7a40f
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2020-08-10 23:36:38 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+class App extends React.Component{
+  render (){
+    return(       
+      <View style={{justifyContent:'center',alignItems:'center'}}>
+      <Text>Totle Pay</Text>
+      </View>     
+    );
+  }
+}
+export default App;
