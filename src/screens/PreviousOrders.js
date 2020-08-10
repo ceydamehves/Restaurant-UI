fb7b760631abb85504bc72dbfb327c03d693a793
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2020-08-11 00:55:05 +0300

    Header added

--- src/screens/PreviousOrders.js
+++ src/screens/PreviousOrders.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
-
+import {Header} from 'react-native-elements';
 class PreviousOrders extends React.Component {
   render() {
     return (
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <View style={{flex:1}}>
+            <Header
+                placement="center"
+                containerStyle={{backgroundColor: '#2F364E', height:120}}
+                centerComponent={{ text: 'PREVIOUS ORDERS', style: {marginTop:40, color: '#fff', fontSize:16 }}}/>
+                <View style={{ flex:1 , alignItems:'center', justifyContent:'center'}}>
             <Button
             title="Go Search"
             onPress={() => this.props.navigation.navigate('SearchBusinesses')}
-            />
+            /></View>
         </View>
+
     );
   }
 }
--- src/screens/SearchBusinesses.js
+++ src/screens/SearchBusinesses.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import {Header} from 'react-native-elements';
 class SearchBusinesses extends React.Component{
   render (){
     return( 
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <View style={{flex:1}}>
+            <Header
+                placement="center"
+                containerStyle={{backgroundColor: '#2F364E', height:120}}
+                centerComponent={{ text: 'SEARCH BUSINESSES', style: {marginTop:40, color: '#fff', fontSize:16 }}}/>
+            <View style={{ flex:1 , alignItems:'center', justifyContent:'center'}}>
             <Text>Search Businesses</Text>
+            </View>
         </View>     
     );
 
