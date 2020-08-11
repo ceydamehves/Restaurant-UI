908ecaa96a8e8b391ce4e1ded60111f67d8bfaad
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2020-08-12 00:24:33 +0300

    SearchBusinesses Screen design

--- src/screens/SearchBusinesses.js
+++ src/screens/SearchBusinesses.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {StatusBar, View, Text, FlatList, StyleSheet, Alert, Image, TouchableOpacity } from 'react-native';
 import {Header, SearchBar} from 'react-native-elements';
+
+
 class SearchBusinesses extends React.Component{
+
   state = {
     search: '',
   };
@@ -10,32 +13,75 @@ class SearchBusinesses extends React.Component{
     this.setState({ search });
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      FlatListItems: [
+        { id: '1', name: 'RESTAURANT' , src: require('../asset/res.jpg')},
+        { id: '2', name: 'RESTAURANT 2' , src: require('../asset/res.jpg')},
+        { id: '3', name: 'RESTAURANT 3' , src: require('../asset/res.jpg')},
+        { id: '4', name: 'RESTAURANT 4' , src: require('../asset/res.jpg')},
+        { id: '5', name: 'RESTAURANT 5' , src: require('../asset/res.jpg')},
+        { id: '6', name: 'RESTAURANT 6' , src: require('../asset/res.jpg')},
+        { id: '7', name: 'RESTAURANT 7' , src: require('../asset/res.jpg')}
+      ],
+    };
+  } 
+   
+
   render() {
     const { search } = this.state;
     return( 
+      
         <View style={{flex:1,backgroundColor:'#23283A'}}>
-            <Header
-                placement="center"
-                containerStyle={{
-                backgroundColor: '#2F364E', 
-                height:120,
-                borderBottomColor:'black',
-                borderBottomWidth:1
-                }}
-                centerComponent={{ text: 'SEARCH BUSINESSES', style: {marginTop:40, color: '#fff', fontSize:16 }}}/>
+            <StatusBar backgroundColor="#2F364E" barStyle="light-content" />
+            
             <SearchBar
-              placeholder=""
+              placeholder=" "
               onChangeText={this.updateSearch}
-              value={search}
+              name={search}
               lightTheme
               inputContainerStyle={{backgroundColor:'white', marginTop:10,height:30,borderRadius:10}}
-              containerStyle={{alignItems:'center',backgroundColor:'transparent', borderTopWidth:0, borderBottomWidth:0}}
-            />            
-            <View style={{ flex:1 , alignItems:'center', justifyContent:'center'}}>
-            </View>
+              containerStyle={{alignItems:'center',backgroundColor:'transparent', borderTopWidth:0, borderBottomWidth:0}}/>   
+
+            <View style={{flex:1}}>
+            <FlatList 
+              data={this.state.FlatListItems}
+              renderItem={({ item }) => (
+                <View style={styles.deneme}>
+                <TouchableOpacity
+                onPress={() => this.props.navigation.navigate('Restaurant')}>
+                  <Image style={{width:350,
+                                 height:110,borderTopLeftRadius:10,borderTopRightRadius:10}}
+                      source={item.src}/>
+                  
+                  <Text style={styles.item}>{item.name}</Text>
+                  </TouchableOpacity>
+                </View> 
+              )}/>           
+              </View>              
         </View>     
     );
 
   }
 }
 export default SearchBusinesses;
+
+const styles = StyleSheet.create({
+  deneme:{
+    backgroundColor:'#353A50',
+    borderRadius:10,
+    borderWidth:2,
+    borderColor:'#353A50',
+    marginTop:25,
+    marginLeft:20,
+    marginRight:20,
+  },
+  item: {
+    alignSelf:'center',
+    marginTop:10,
+    marginBottom:10,
+    fontSize: 16,    
+    color:'white'
+  },
+});
